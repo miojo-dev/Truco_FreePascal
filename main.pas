01fd7419c3a0df9b8ee998687b376980b2bdd139
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-03-24 20:01:48 -0300

    feat/card types

--- main.pas
+++ main.pas
@@ -1,1 +1,24 @@
+program Truco_Paulista;
+
+{
+function NameFunction
+
+var nameVar
+
+type TNameTipe
+}
+
+const
+  deckSize = 40;
+  handSize  = 3;
+
+type
+  TSuit = (nClubs, nHearts, nSpades, nDiamonds);
+
+  TCard = record
+    value : integer;
+    suit : TSuit;
+    isManilha : Boolean;
+and;
+
 
