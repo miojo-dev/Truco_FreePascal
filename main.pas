12ed462a5cbe20719dbf275b2bfff48951016af6
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-03-24 20:57:07 -0300

    feat/force conversor for cards

--- main.pas
+++ main.pas
@@ -21,4 +21,22 @@ type
     isManilha : Boolean;
 and;
 
+{ ===== Utils ===== }
 
+{convert card num to truco base force of the card}
+function NumToForce(v : integer) : integer;
+begin
+  case v of
+    4 : Result := 1;
+    5 : Result := 2;
+    6 : Result := 3;
+    7 : Result := 4;
+    10: Result := 5;
+    11: Result := 6;
+    12: Result := 7;
+    1 : Result := 8;
+    2 : Result := 9;
+    3 : Result := 10;
+    else Result := 0;
+  end;
+end;
